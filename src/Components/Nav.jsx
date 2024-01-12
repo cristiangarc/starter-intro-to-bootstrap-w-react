f16ec7a184ae0328aee0b2cfed4929d3ac928380
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-11 23:37:56 -0500

    added a grid layout for the posts

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,70 +4,81 @@
 // <!-- ************************** -->
 
 export default function Nav() {
-  return (
-    <nav className="">
-      <div className="">
-        <a className="">Rosier</a>
-        <button
-          className=""
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className=""></span>
-        </button>
-        <div className="" id="navbarSupportedContent">
-          <ul className="">
-            <li className="">
-              <a className="" aria-current="page">
-                Home
-              </a>
-            </li>
-            <li className="">
-              <a className="">Featured</a>
-            </li>
-            <li className="">
-              <a
-                className=""
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Learn More
-              </a>
-              <ul className="" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="">About</a>
-                </li>
-                <li>
-                  <a className="">Contact</a>
-                </li>
-                <li>
-                  <hr className="" />
-                </li>
-                <li>
-                  <a className="">Surprise me!</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form className="">
-            <input
-              className=""
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand">Rosier</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Featured</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                id="navbarDropdown"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Learn More
+                            </a>
+                            <ul
+                                className="dropdown-menu"
+                                aria-labelledby="navbarDropdown"
+                            >
+                                <li>
+                                    <a className="dropdown-item">About</a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item">Contact</a>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider" />
+                                </li>
+                                <li>
+                                    <a className="dropdown-item">
+                                        Surprise me!
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit"
+                        >
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
 }
